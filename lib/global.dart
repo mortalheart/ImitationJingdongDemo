b67a452f90352394f0351ddcd3c9a4d0d91cb126
mortalheart <qingfeng.yest@gmail.com>
2023-12-18 14:40:50 +0800

    添加下拉刷新

---
+++ lib/global.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:get/get.dart';
+class Global {
+  static Future<void> init() async {
+    WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized(); // 等待Flutter初始化完成
+    FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+    systemStyle();
+  }
+
+  static void systemStyle() {
+    if (GetPlatform.isMobile) {
+      // 屏幕方向 竖直上
+      SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+    }
+    if (GetPlatform.isAndroid) {
+      // 去除顶部系统下拉和底部系统按键
+      // SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
+      // 去掉底部系统按键
+      // SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+      //     overlays: [SystemUiOverlay.bottom]);
+
+      // 自定义样式
+      SystemUiOverlayStyle systemUiOverlayStyle = const SystemUiOverlayStyle(
+        // 顶部状态栏颜色
+        statusBarColor: Colors.transparent,
+        // 该属性仅用于 iOS 设备顶部状态栏亮度
+        statusBarBrightness: Brightness.light,
+        // 顶部状态栏图标的亮度
+        statusBarIconBrightness: Brightness.dark,
+
+        // 底部状态栏与主内容分割线颜色
+        systemNavigationBarDividerColor: Colors.transparent,
+        // 底部状态栏颜色
+        systemNavigationBarColor: Colors.white,
+        // 底部状态栏图标样式
+        systemNavigationBarIconBrightness: Brightness.dark,
+      );
+      SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
+    }
+  }
+}
